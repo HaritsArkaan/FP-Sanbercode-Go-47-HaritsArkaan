a66690191f107a082cf323a261e66c386d609856
Harits Arkaan <arkaanputra73@gmail.com>
2023-07-27 19:57:39 +0700

    menambahkan config

---
+++ config/db.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"FP-Sanbercode-Go-47-HaritsArkaanPutranto/models"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDataBase() *gorm.DB {
+	username := "root"
+	password := "Kanarkaan73"
+	host := "tcp(127.0.0.1:3306)"
+	database := "database_handphone"
+
+	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&models.Brand{}, &models.Phone{}, &models.User{})
+
+	return db
+}
